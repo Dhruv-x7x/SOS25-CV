2e370d314a34e8e1dcde6ba589c1e31cfdbb0c97
unknown <dhruv7meena@gmail.com>
2025-06-20 22:03:29 +0530

    softmax

--- week_1-2/assignment1/cs231n/classifiers/softmax.py
+++ week_1-2/assignment1/cs231n/classifiers/softmax.py
@@ -2,61 +2,77 @@ import numpy as np
 from random import shuffle
 
 def softmax_loss_naive(W, X, y, reg):
-  """
-  Softmax loss function, naive implementation (with loops)
+    loss = 0.0
+    dW = np.zeros_like(W)
+    N = X.shape[0]
+    C = W.shape[1]
 
-  Inputs have dimension D, there are C classes, and we operate on minibatches
-  of N examples.
+    for i in range(N):
+        scores = X[i].dot(W)
+        # Stabilize scores
+        max_score = np.max(scores)
+        scores -= max_score
+        
+        # Compute softmax probabilities
+        exp_scores = np.exp(scores)
+        sum_exp = np.sum(exp_scores)
+        probs = exp_scores / sum_exp
+        
+        # Compute loss
+        correct_class_prob = probs[y[i]]
+        loss += -np.log(correct_class_prob)
+        
+        # Compute gradient
+        for j in range(C):
+            indicator = 1 if j == y[i] else 0
+            dW[:, j] += (probs[j] - indicator) * X[i]
 
-  Inputs:
-  - W: A numpy array of shape (D, C) containing weights.
-  - X: A numpy array of shape (N, D) containing a minibatch of data.
-  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
-    that X[i] has label c, where 0 <= c < C.
-  - reg: (float) regularization strength
-
-  Returns a tuple of:
-  - loss as single float
-  - gradient with respect to weights W; an array of same shape as W
-  """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
-
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
-
-  return loss, dW
+    # Average loss/gradient
+    loss /= N
+    dW /= N
+    
+    # Regularization
+    loss += 0.5 * reg * np.sum(W * W)
+    dW += reg * W
+    
+    return loss, dW
 
 
 def softmax_loss_vectorized(W, X, y, reg):
-  """
-  Softmax loss function, vectorized version.
+    """
+    Softmax loss function, vectorized version.
+    
+    Inputs:
+    - W: Weights (D, C)
+    - X: Data (N, D)
+    - y: Labels (N,)
+    - reg: Regularization strength
+    
+    Returns:
+    - loss: Scalar
+    - dW: Gradient (D, C)
+    """
+    num_train = X.shape[0]
+    
+    # Compute scores and stabilize
+    scores = X.dot(W)
+    max_scores = np.max(scores, axis=1, keepdims=True)
+    exp_scores = np.exp(scores - max_scores)
+    
+    # Softmax probabilities
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    
+    # Loss calculation
+    correct_probs = probs[np.arange(num_train), y]
+    loss = -np.sum(np.log(correct_probs)) / num_train
+    loss += 0.5 * reg * np.sum(W * W)
+    
+    # Gradient calculation
+    dscores = probs.copy()
+    dscores[np.arange(num_train), y] -= 1
+    dW = X.T.dot(dscores) / num_train
+    dW += reg * W
+    
+    return loss, dW
 
-  Inputs and outputs are the same as softmax_loss_naive.
-  """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
-
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  pass
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
-
-  return loss, dW
 
